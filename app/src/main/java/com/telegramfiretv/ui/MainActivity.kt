9bc760d2251b7676a6481a48feb788dfa8cf41c4
S@m <80917294+Samusacmd@users.noreply.github.com>
2026-05-30 18:47:44 +0200

    Update MainActivity.kt

--- app/src/main/java/com/telegramfiretv/ui/MainActivity.kt
+++ app/src/main/java/com/telegramfiretv/ui/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.telegramfiretv.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.KeyEvent
 import androidx.fragment.app.FragmentActivity
 import com.telegramfiretv.R
 
@@ -8,5 +10,13 @@ class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_MENU) {
+            startActivity(Intent(this, SettingsActivity::class.java))
+            return true
+        }
+        return super.onKeyDown(keyCode, event)
     }
 }
