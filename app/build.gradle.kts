5165e76971e50bd73b4a2cbbe92503b9f125944b
S@m <80917294+Samusacmd@users.noreply.github.com>
2026-06-20 12:24:07 +0200

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.leanback:leanback:1.0.0")
     implementation("androidx.media3:media3-exoplayer:1.4.1")
+    implementation("androidx.media3:media3-datasource:1.4.1")
     implementation("androidx.media3:media3-ui:1.4.1")
     implementation("com.airbnb.android:lottie:6.4.0")
 }
